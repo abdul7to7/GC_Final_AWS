bb6936ebc820b22fd6e8b3d8b1c655b05782b79d
unknown <abdul7to7@gmail.com>
2024-11-10 14:34:11 +0530

    server change

--- frontend/login.js
+++ frontend/login.js
@@ -1,4 +1,4 @@
-const server = "http://localhost:3000";
+const server = "http://13.233.144.241:3000";
 
 document.getElementById("loginForm").addEventListener("submit", async (e) => {
   e.preventDefault();
